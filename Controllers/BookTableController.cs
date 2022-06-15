6de616c644d44b598f29fa2b0d768c36ad61be01
hinh.le <levhinhvh@gmail.com>
2022-06-15 21:26:33 +0700

    Post BookTable

---
+++ Controllers/BookTableController.cs
@@ -0,0 +1,61 @@
+using Demeter.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Data;
+namespace Demeter.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BookTableController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public BookTableController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult Post(BookTableDto request)
+        {
+            var sqlconnectstring =  _configuration.GetConnectionString("DefaultConnection");
+            var connection = new MySqlConnection(sqlconnectstring);
+            connection.Open();
+            if (connection.State == ConnectionState.Open)
+            {
+                var rows_affected = 0;
+                using var command = new MySqlCommand();
+                command.Connection = connection;
+
+                string queryString = @"INSERT INTO booktable (Name, Email, Phone, Date, Time, NumPeople, Message) VALUES (@Name, @Email, @Phone, @Date, @Time, @NumPeople, @Message);
+                                select last_insert_id();";
+
+                command.CommandText = queryString;
+                command.Parameters.AddWithValue("@Name", request.Name);
+                command.Parameters.AddWithValue("@Email", request.Email);
+                command.Parameters.AddWithValue("@Phone", request.Phone);
+                command.Parameters.AddWithValue("@Date", request.Date);
+                command.Parameters.AddWithValue("@Time", request.Time);
+                command.Parameters.AddWithValue("@NumPeople", request.NumPeople);
+                command.Parameters.AddWithValue("@Message", request.Message);
+
+                try
+                {
+                    rows_affected = command.ExecuteNonQuery();
+                    if (rows_affected > 0)
+                    {
+                        connection.Close();
+                        return Ok();
+                    }
+                    return BadRequest();
+
+                }
+                catch (System.Exception)
+                {
+                    return BadRequest();
+                }
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ DTOs/BookTableDro.cs
@@ -0,0 +1,15 @@
+namespace Demeter.DTOs;
+
+public class BookTableDto
+{
+    public string? Name { get; set; }
+    public string? Email { get; set; }
+    public long? Phone { get; set; }
+    public int? Date { get; set; }
+    public string? Time { get; set; }
+    public int? NumPeople { get; set; }
+    public string? Message { get; set; }
+
+
+
+}

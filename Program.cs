03c0141b662f1fb53fc9ad27d561acaa3c0ecd5f
hinh.le <levhinhvh@gmail.com>
2022-06-16 11:41:18 +0700

    Tested using swagger in deployment

--- Program.cs
+++ Program.cs
@@ -10,12 +10,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-Console.WriteLine($"Env: {app.Environment.IsDevelopment()}");
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseCors(x => x.AllowAnyOrigin()
      .AllowAnyMethod()
      .AllowAnyHeader());
